2972547c55dca8005be0eaaf088c892e8a28265d
yanngodeau <yann.godeau@gmail.com>
2017-05-05 22:01:46 +0200

    ButtonSensor

---
+++ PET_Unity/Assets/Scripts/Interface/ChangeButtonColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButtonColor : MonoBehaviour
+{
+    public bool ButtonOn = false;
+    public Button MyButton;
+    public Color ColorOff;
+    public Color ColorOn;
+
+    public void BeenClicked()
+    {
+        ButtonOn = !ButtonOn;
+        MyButton.image.color = ButtonOn ? ColorOn : ColorOff;
+    }
+}
